793c460f1410c6b437824b142fe02ce35729deb9
AndrewKim-Leekim <leekimelect@gmail.com>
2025-10-28 12:15:45 +0900

    first commit

---
+++ src/main/java/com/mycompany/passcodedemo/PassCodeDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.passcodedemo;
+
+/**
+ *
+ * @author Andrew
+ */
+public class PassCodeDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
